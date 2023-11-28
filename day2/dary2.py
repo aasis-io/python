dae31368fbc80b7b4a962f38f09e926c242947dd
aasis-io <heyashishthapa@gmail.com>
2023-11-28 12:01:16 +0545

    day 2 added

---
+++ day2/dary2.py
@@ -0,0 +1,14 @@
+print("Hello World!", sep='')
+print("I am Ashish!")
+
+print("Hello World!", end='')
+print("I am Ashish!")
+
+a=9
+b=6
+
+c= a+b
+
+print(f"The Sum of {a} and {b} is {c}") 
+
+print(type(a))
