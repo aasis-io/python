7f7146a6f06271a1fb89057579cd58b6fe1c7a47
aasis-io <heyashishthapa@gmail.com>
2023-12-11 12:03:18 +0545

    day 6 in python completed

---
+++ data_type_cont.py
@@ -0,0 +1,20 @@
+# fruits = ["apple", "orange", "banana"]
+
+# fruits.append("pen")
+
+# fruits.insert(0, "pineapple")
+
+# print(fruits)
+
+# numbers = [3,4,5,6,7,8,1,2]
+
+# numbers.sort()
+
+# print(numbers.count(2))
+
+fruits = ["apple", "orange", "banana"]
+print(fruits)
+fruits.append("pineapple")
+print(fruits)
+fruits.remove("orange")
+print(fruits)
