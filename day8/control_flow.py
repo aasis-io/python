0e015aab44e58e1923e19f5ba4a7919948db68a3
aasis-io <heyashishthapa@gmail.com>
2023-12-18 12:06:10 +0545

    day 8 in python added

---
+++ day8/control_flow.py
@@ -0,0 +1,52 @@
+# price = 1400
+# discount = 0
+
+# if price > 1500:
+#     discount = price * 0.15
+
+# elif price > 2500 < 3000:
+#     discount = price * 0.2
+
+# else:
+#     discount = price * 0.05
+
+
+# print("Total price is", price - discount)
+
+
+percentage = 65
+
+# if percentage > 80:
+#     print("Distinction")
+
+# elif percentage < 80 > 60:
+#     print("first Division")
+
+# elif percentage < 60 > 40:
+#     print("second Division")
+
+# elif percentage < 40 > 30:
+#     print("third Division")
+
+# else:
+#     print("Fail")
+
+
+
+# match percentage:
+#     case 80:
+#         print("Distinction")
+    
+#     case 60:
+#         print("First Division")
+
+#     case 50:
+#         print("Second Division")
+
+#     case 40:
+#         print("Third Division")
+
+#     case _:
+#         print("Fail")
+
+
