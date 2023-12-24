ef4e0339cf3604f5b15d194ddd63e26fdd8adc2a
aasis-io <heyashishthapa@gmail.com>
2023-12-24 19:34:20 +0545

    learned function in python

---
+++ day10/function.py
@@ -0,0 +1,29 @@
+# def printName():
+#     print("My first function in python")
+
+# printName()
+
+
+# def substractValues(a, b):
+#     print(a-b)
+
+
+# def addition(a, b):
+#     print(a+b)
+
+
+# def product(a, b):
+#     print(a*b)
+
+
+# def division(a, b):
+#     print(a/b)
+
+# a=int(input("First Number:"))
+# b=int(input("Second Number:"))
+
+# substractValues(a,b)
+# addition(a,b)
+# product(a,b)
+# division(a,b)
+
