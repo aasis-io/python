ac435a704411d5a22fcfa017f6a57c482a608454
aasis-io <heyashishthapa@gmail.com>
2023-12-25 12:21:14 +0545

    day 11 completed

--- day11/argument.py
+++ day11/argument.py
@@ -26,17 +26,43 @@
 # perform_math_operation(n1,n2,oper)
 
 
-def multiply(*args):
-    total_multiply = 1
+# def multiply(*args):
+#     total_multiply = 1
 
-    for number in args:
-        total_multiply *= number
+#     for number in args:
+#         total_multiply *= number
 
-    return total_multiply
+#     return total_multiply
 
-total_value = multiply(1, 2, 3)
+# total_value = multiply(1, 2, 3)
 
-print(total_value)
+# print(total_value)
 
 
 
+# def greet(**kwargs):
+#     print(type(kwargs))
+
+# greet(name1="John", name2="Doe")
+
+
+# def total_marks(math, english, **kwargs):
+#     total = math + english
+
+#     for subject, mark in kwargs.items():
+#         total += mark
+    
+#     print("Total marks:", total);
+
+# total_marks(math = 23, english = 45, opt_math = 10, account=20)
+
+
+def print_info(name, **args):
+    print("Name:", name)
+
+    for detail, value in kwargs.items():
+        print(f"{detail}:", value)
+
+
+
+print_info(name = "ashish", age = 23, phone = 9840033688)
