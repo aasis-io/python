a3fc32e621e76ce8c8fe54d91964e674729cca44
aasis-io <heyashishthapa@gmail.com>
2023-12-26 12:02:54 +0545

    day 12th calculator

--- day12/classes.py
+++ day12/classes.py
@@ -1,15 +1,33 @@
-class Student:
-    sid = 1
-    name = "John"
-    address = "NYC"
-    phone = "9856789987"
+# class Student:
 
-    def print_details(self):
-        print("ID:", self.sid)
-        print("Name:", self.name)
-        print("Address:", self.address)
-        print("Phone:", self.phone)
+#     def __init__(self, student_id, name, add, phone):
+#         self.student_id = student_id
+#         self.name = name
+#         self.add = add
+#         self.phone = phone
 
-student1 = Student()
-student1.print_details()
+#     def print_details(self):
+#         print("ID:", self.student_id)
+#         print("Name:", self.name)
+#         print("Address:", self.add)
+#         print("Phone:", self.phone)
+
+# student1 = Student(1, "John", "ktm", "9878990098")
+# student1.print_details()
+
+class Calculator:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def printCalculation(self):
+        print("Addition = ", self.a+self.b)
+        print("Subtraction = ", self.a-self.b)
+        print("Division = ", self.a/self.b)
+        print("Multiplication = ", self.a*self.b)
+
+calc = Calculator(6, 3)
+
+calc.printCalculation()
 
