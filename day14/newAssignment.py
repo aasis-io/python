60c852da686893615fcde34219209dd31ad2e85b
Ashish Thapa <heyashishthapa@gmail.com>
2024-01-02 12:07:27 +0545

    inhertence learned in python

--- day14/newAssignment.py
+++ day14/newAssignment.py
@@ -1,30 +1,46 @@
-class Designer:
-    def __init__(self, name, address, salary, tool):
+class Employee:
+    def __init__(self, name, address, salary):
         self.name = name
         self.address = address
         self.salary = salary
+
+    def print_details(self):
+        print("Name:" + self.name)
+        print("Address:" + self.address)
+        print("Salary:" + str(self.salary))
+
+class Designer(Employee):
+    def __init__(self, name, address, salary, tool):
+        super().__init__(name, address, salary)
         self.tool = tool
 
 
     def print_details(self):
-        print("Name:" + self.name)
-        print("Address:" + self.address)
-        print("Salary:" + str(self.salary))
+        super().print_details()
         print("Tool:" + self.tool)
 
 
-class Developer:
+    # def print_details(self):
+    #     print("Name:" + self.name)
+    #     print("Address:" + self.address)
+    #     print("Salary:" + str(self.salary))
+    #     print("Tool:" + self.tool)
+
+
+class Developer(Employee):
     def __init__(self, name, address, salary, programming_lang):
-        self.name = name
-        self.address = address
-        self.salary = salary
+        super().__init__(name, address, salary)
         self.programming_lang = programming_lang
 
     def print_details(self):
-        print("Name:" + self.name)
-        print("Address:" + self.address)
-        print("Salary:" + str(self.salary))
+        super().print_details()
         print("Programming Language:" + self.programming_lang)
+
+    # def print_details(self):
+    #     print("Name:" + self.name)
+    #     print("Address:" + self.address)
+    #     print("Salary:" + str(self.salary))
+    #     print("Programming Language:" + self.programming_lang)
 
 
 designer1 = Designer(
