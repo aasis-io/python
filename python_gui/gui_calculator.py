aaee8d1cb5d3689a709f5e9436467d5be640368c
Ashish Thapa <heyashishthapa@gmail.com>
2024-02-08 11:29:26 +0545

    gui python

--- python_gui/gui_calculator.py
+++ python_gui/gui_calculator.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import font
 
 
 window = tk.Tk()
@@ -20,9 +21,14 @@ def  my_button_handler():
     firstValue = int(my_textfield.get())
     secondValue = int(my_textfield2.get())
     print("Sum =", firstValue + secondValue)
+    result_label.configure(text="Result=" + str(firstValue + secondValue))
 
 
-my_button = ttk.Button(master = window, text="Click Me", command = my_button_handler)
+
+my_button = ttk.Button(master = window, text="Add", command = my_button_handler)
 my_button.place(x = 180, y = 180)
+
+result_label = ttk.Label(master = window, text="Result=", font = font.Font(size=16))
+result_label.place(x = 180, y = 220)
 
 window.mainloop()
