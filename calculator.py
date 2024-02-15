002964304f83a4f85b3c0dfbbeb4af1800cd4df2
Ashish Thapa <heyashishthapa@gmail.com>
2024-02-15 11:20:04 +0545

    developing calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+
+window = tk.Tk()
+
+buttons = [
+    '7', '8', '9', '+',
+    '4', '5', '6', '-',
+    '1', '2', '3', '*',
+    '0', 'C', '.', '/',
+    '='
+];
+
+
+window.mainloop()
