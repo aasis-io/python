cca747426afa333ec72f1eb2cb781292b9317c49
Ashish Thapa <heyashishthapa@gmail.com>
2024-02-16 12:10:22 +0545

    make it green

--- automation/sharesansar/sharesansar.py
+++ automation/sharesansar/sharesansar.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import json
 import time
 
 driver = webdriver.Chrome()
@@ -9,13 +10,23 @@ driver.get("https://www.sharesansar.com/existing-issues")\
 table_element = driver.find_element(By.ID, 'myTableEip')
 
 row_elements = table_element.find_elements(By.TAG_NAME, 'tr')
-
+table_data = []
 for row in row_elements[1:]:
     data_elements = row.find_elements(By.TAG_NAME, 'td')
-    for data in data_elements:
-        print(data.text)
+    # for data in data_elements:
+    #     print(data.text)
+    ipo = {
+        'S.N' : data_elements[0].text,
+        'Symbol' : data_elements[1].text,
+        'Company' : data_elements[2].text,
+        'Unit' : data_elements[3].text
+    }
 
+    table_data.append(ipo)
 
-time.sleep(50)
+with open('ipo_data.json','w') as f: #automatically closes the file after use
+    json.dump(table_data, f, indent=2) #writes the data to ipo_data.json froom table_data
+
+# time.sleep(50)
 
 driver.close()
